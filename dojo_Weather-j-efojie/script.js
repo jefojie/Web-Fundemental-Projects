6616929c645c814474a860bca823cce5441dcb90
Joshua Efojie <120279162+jefojie@users.noreply.github.com>
2023-05-04 10:46:51 -0400

    Add files via upload

---
+++ dojo_Weather-j-efojie/script.js
@@ -0,0 +1,13 @@
+console.log("script of java is working....");
+
+
+var cookies = document.querySelector(".cookie")
+
+
+function load() {
+    alert("Weather is loading...");
+}
+
+function accept() {
+    cookies.remove();
+}
