bf6a94f3790cfbb977c2b6fbb38389bd17aa0634
Joshua Efojie <120279162+jefojie@users.noreply.github.com>
2023-05-04 10:46:25 -0400

    Add files via upload

---
+++ Likes_Joshua_Efojie/script.js
@@ -0,0 +1,9 @@
+console.log("Yes my javascript is working")
+
+var likeCount = 3
+var likeSpan1 = document.querySelector("#post-1");
+
+function like() {
+    likeCount++;
+    likeSpan1.innerText = likeCount + "like(s)"
+}
